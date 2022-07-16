e1806f02c20a16dfe20014bcd13380d7e58bc0ec
Alexandresfi <alexandre.sfi@hotmail.com>
2022-07-16 13:13:31 -0300

    project created and frist migration of users

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'escolaorganizada',
+  database: 'escolaorganizada',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
