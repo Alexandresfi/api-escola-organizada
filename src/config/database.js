b453a156a31cffbc98e63c4353e6b86e25c0ccf7
Alexandresfi <alexandre.sfi@hotmail.com>
2022-08-02 20:49:02 -0300

    fix environment variable

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'escolaorganizada',
-  database: 'escolaorganizada',
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DATABASE,
   define: {
     timespamps: true,
     underscored: true,
