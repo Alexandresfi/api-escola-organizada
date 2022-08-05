9c18843e0ff99586b968fe4b8fa1eeab9a0cd156
Alexandresfi <alexandre.sfi@hotmail.com>
2022-08-04 21:28:00 -0300

    🛠 FIX config connecting database heroku

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,12 @@ module.exports = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DATABASE,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   define: {
     timespamps: true,
     underscored: true,
